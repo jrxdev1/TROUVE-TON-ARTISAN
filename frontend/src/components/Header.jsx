a20626c0c5b7a7f8039069b3f7685a8e9b9f2788
Jrx <jrxdeveloppement@gmail.com>
2026-01-24 13:06:53 +0100

    style/1

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -38,37 +38,37 @@ function Header() {
     };
 
     return (
-        <header>
-            <div>
-            <img src="../src/assets/images/Logo.png" alt="" onClick={() => navigate("/")}/>
-            </div>
-            
-            <div>
-                <form onSubmit={handleSearch}>
-                    <input type="text" name="" id="" placeholder='Rechercher un artisan' value={search} onChange={(e) => setSearch(e.target.value)} />
-                    <button type='submit'>Rechercher</button>
-                </form>
-            </div>
+        <nav className='navbar navbar-expand-lg bg-light'>
+            <div className='container-fluid'>
+                <img src="../src/assets/images/Logo.png" alt="" onClick={() => navigate("/")} className='col-sm-2'/>
+                
+                <form onSubmit={handleSearch} className='d-flex ms-3'>
+                    <input type="text" className='form-control me-2' placeholder='Rechercher un artisan' value={search} onChange={(e) => setSearch(e.target.value)} />
+                    <button type='submit' className='btn btn-outline-success'>Rechercher</button>
+                </form>    
 
-            <div>
-                <nav>
-                    {categories.map(categorie => (
-                        <div key={categorie.id_categorie} className='menu-categories'>
-                            <span className='categories-title' onClick={()=> toggleCategorie(categorie.id_categories)}>{categorie.nom_categories}</span>
-                            {openCategorie === categorie.id_categories && (
-                            <div className='dropdown'>
-                                {categorie.Specialites.map(spe => (
-                                    <Link key={spe.id_specialites} to={`/specialite/${spe.id_specialites}`}>
-                                    {spe.nom_specialites}   
-                                    </Link>
-                                ))}
-                            </div>
-                            )}
-                        </div>
-                    ))}
-                </nav>
+                <div className='collapse navbar-collapse'>
+                    <ul className='nav-item dropdown ms-auto'>
+                        {categories.map(categorie => (
+                            <li key={categorie.id_categorie} className='nac-item dropdown'>
+                                <span className='nav-link dropdown-toggle' onClick={()=> toggleCategorie(categorie.id_categories)}>{categorie.nom_categories}</span>
+                                {openCategorie === categorie.id_categories && (
+                                <ul className='dropdown-menu show'>
+                                    {categorie.Specialites.map(spe => (
+                                        <li key={spe.id_specialites}>
+                                        <Link to={`/specialite/${spe.id_specialites}`} className='dropdown-item'>
+                                        {spe.nom_specialites}   
+                                        </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </header>
+        </nav>
     );
 };
 
